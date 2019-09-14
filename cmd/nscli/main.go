dbe5fb36ad8747cf84a2d6305aa124841c6cf7c2
zhiqiang <745124335@qq.com>
2019-09-14 10:37:52 +0800

    fix error

--- cmd/nscli/main.go
+++ cmd/nscli/main.go
@@ -11,7 +11,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
 	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
-	app "github.com/zhiqiang-bianjie/cmp-chain/"
+	app "github.com/zhiqiang-bianjie/cmp-chain"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	amino "github.com/tendermint/go-amino"
